6bdcf3466ee1e40bfde51dc0e5d16ad600208454
Sujal <morwanisujal875@gmail.com>
2024-12-05 17:21:01 +0530

    crud operation started for task manager

--- controller/task.js
+++ controller/task.js
@@ -1,7 +1,15 @@
 const Task = require("../models/task.js");
 const { StatusCodes } = require("http-status-codes");
-const getAlltasks = (req, res) => {
-  res.send("Getting All Items");
+const getAlltasks = async (req, res) => {
+  try {
+    // const
+    const tasks = await Task.find({});
+    return res.status(StatusCodes.OK).json({ tasks });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
 };
 
 const createtask = async (req, res) => {
@@ -15,16 +23,64 @@ const createtask = async (req, res) => {
   }
 };
 
-const gettask = (req, res) => {
-  res.json({ id: req.params.id });
+const gettask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({
+      _id: taskID,
+    });
+    if (!task) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: "Task not found",
+      });
+    }
+    return res.status(StatusCodes.OK).json({ task });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message,
+    });
+  }
 };
 
-const updatetask = (req, res) => {
-  res.send("updating  task");
+const updatetask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndUpdate(
+      {
+        _id: taskId,
+      },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    return res.status(StatusCodes.OK).json({
+      message: "Task updated successfully",
+    });
+  } catch (error) {}
 };
 
-const deletetask = (req, res) => {
-  res.send("delete task");
+const deletetask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({
+      _id: taskID,
+    });
+
+    if (!task) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: "No Task Found",
+      });
+    }
+    return res.status(StatusCodes.OK).json({
+      message: "Task deleted successfully",
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message,
+    });
+  }
 };
 module.exports = {
   getAlltasks,
