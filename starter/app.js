df84a123cb09bb20fd8110b71b023d58cf47d7e3
SujalGeek <morwanisujal875@gmail.com>
2025-01-11 20:17:17 +0530

    added some routes

--- starter/app.js
+++ starter/app.js
@@ -1,19 +1,29 @@
 require("dotenv").config();
 require("express-async-errors");
+const cors = require("cors");
 const userRoutes = require("./routes/main.js");
 
 const express = require("express");
 const app = express();
 
-// const notFoundMiddleware = require("./middleware/not-found");
-// const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.static("./public"));
-app.use(express.json()); // Add this line to parse JSON bodies
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+); // Add this line to parse JSON bodies
 // middleware
-app.use("/", userRoutes);
-// app.use(notFoundMiddleware);
-// app.use(errorHandlerMiddleware);
+app.use("/api/v1", userRoutes);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
--- starter/controllers/main.js
+++ starter/controllers/main.js
@@ -25,9 +25,23 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+  // console.log(req.headers);
+  const token = authHeader.split(" ")[1];
+  // console.log(token);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access the route", 401);
+  }
+
   const luckynumber = Math.floor(Math.random() + 100);
   res.status(200).send({
-    message: "Hello",
+    message: `Hello, ${decoded.username}`,
     secret: `Here is your authorized data ${luckynumber}`,
   });
 };
